a6d24406c54fe1d1f6f389f2f5f9550536c7561f
Aaronerar <aaronnader@hotmail.com>
2023-02-03 15:37:09 -0300

    Teste 1

---
+++ index.js
@@ -0,0 +1,13 @@
+let meuNome = "Calango";
+let sobreNome = "Guitarrista";
+let Idade = 15;
+let ehBonito = true;
+let diaEspecial = 1;
+
+console.log("meuNome")
+console.log("sobreNome")
+console.log("Idade")
+console.log("ehBonito")
+console.log("diaEspecial");
+
+console.log("Meu pai não tocou piano ontem porque estava muito cansado, mas vai começar a tocar dia " + diaEspecial + " de fevereiro");
